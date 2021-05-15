3ec45a5f61f20ca94d9ae8fe3bcc430dc451ee94
Nicola Cimmino <nicola.cimmino@gmail.com>
2021-05-15 11:37:16 +0200

    remove .build

--- .build/core/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-c:\Users\nicola\git\EIPLogger\.build\core\libb64\cencode.c.o: \
- C:\Users\nicola\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4\cores\esp32\libb64\cencode.c \
- C:\Users\nicola\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.4\cores\esp32\libb64\cencode.h
